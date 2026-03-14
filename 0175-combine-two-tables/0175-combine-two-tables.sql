4b8c41a66629e85e637029249a52b8744a8b9dd1
Jayesh Mali <jaymali2107@gmail.com>
2026-03-14 18:52:32 +0530

    Time: 352 ms (98.34%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,3 +1,4 @@
-select p.firstName,p.lastName,o.city,o.state
-from Person as p
-left join Address as o on p.personId = o.personId;
+select p.firstName , p.lastName , a.city , a.state 
+from Person p
+left join Address a
+on p.personId = a.personId 
