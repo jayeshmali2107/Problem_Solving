d35a5b777c76fe809c0bc1aa2036bf7edd01cedc
Jayesh Mali <jaymali2107@gmail.com>
2026-06-11 11:25:27 +0530

    Time: 13 ms (42.11%), Space: 12.4 MB (54.54%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,13 +1,14 @@
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        if x < 0:
+class Solution(object):
+    def isPalindrome(self, x):
+        if(x<0):
             return False
-
-        reverse = 0
-        xcopy = x
-
-        while x > 0:
-            reverse = (reverse * 10) + (x % 10)
-            x //= 10
         
+        reverse = 0    # store reverse value
+        xcopy  = x     # make copy of original value
+
+        while(x>0):
+            reverse = (reverse * 10 ) + (x % 10)            # reverse 
+            x//=10                                          # remove last element
+
         return reverse == xcopy
+        
