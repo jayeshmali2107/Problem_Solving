37549b43c97a6200f4700af8419493654290e1d6
Jayesh Mali <jaymali2107@gmail.com>
2026-08-09 22:28:04 +0530

    Time: 43 ms (83.69%), Space: 29.1 MB (73.6%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -6,29 +6,27 @@ public:
         sort(nums.begin(),nums.end());
 
         for(int i=0; i<n; i++){
-            if(i>0 && nums[i]==nums[i-1]){
+            if(i>0 && nums[i] == nums[i-1]){
                 continue;
             }
             int j = i+1;
             int k = n-1;
-            
+
             while(j<k){
                 int sum = nums[i] + nums[j] + nums[k];
-                if(sum<0){
-                    j++;
-                }
-                else if(sum>0){
+                if(sum > 0){
                     k--;
+                }
+                else if(sum < 0){
+                    j++;
                 }
                 else{
                     ans.push_back({nums[i],nums[j],nums[k]});
                     j++ , k--;
-
                     while(j<k && nums[j] == nums[j-1]){
                         j++;
                     }
                 }
-                
             }
         }
         return ans;
